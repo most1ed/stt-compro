aa4e85f726be238c297093d5ec4617e66396d874
Indra Gunanda <indra.gunanda@gmail.com>
2025-03-03 17:22:55 +0700

    Add files via upload

---
+++ assets/BrandLogo-DuPisva6.js
@@ -0,0 +1,7 @@
+import{d as o,j as e,I as n}from"./index-CA2_JSQU.js";const c=({src:s,alt:t,className:a=""})=>{const{theme:r}=o(),l=r==="dark";return e.jsx("div",{className:`
+        relative flex h-[160px] items-center justify-center p-4
+        ${l?"bg-slate-200/85":"bg-slate-100/85"}
+        hover:bg-slate-200/95 backdrop-blur-sm
+        rounded-lg transition-all duration-300 group shadow-sm
+        ${a}
+      `,children:e.jsx("div",{className:"relative flex h-[80px] w-[180px] items-center justify-center",children:e.jsx(n,{src:s,alt:t,className:"h-full w-full object-contain transition-transform duration-300 group-hover:scale-105"})})})};export{c as B};
